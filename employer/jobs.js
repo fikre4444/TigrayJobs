aedc24ae539dc461529309ee7856733c5f90de4b
= <=>
2024-01-22 08:07:07 +0300

    to merge the two

--- employer/jobs.js
+++ employer/jobs.js
@@ -1,4 +1,4 @@
- // Import the functions you need from the SDKs you need
+ // Import the functions you need from the SDKs you need.
  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
  import { getDatabase, ref, set, push, get, child } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
  import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
